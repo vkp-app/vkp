804a0b2db9496ef91cccf0f3df2b99baceef40fd
Django Cass <django@dcas.dev>
2022-11-09 19:40:36 +1100

    [web] disabled apollo client-side caching

--- web/src/graph/index.ts
+++ web/src/graph/index.ts
@@ -2,6 +2,19 @@ import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const Client = new ApolloClient({
 	uri: "/api/v1/query",
-	cache: new InMemoryCache()
+	cache: new InMemoryCache(),
+	// disable caching so that
+	// we always get up-to-date information
+	// from the API
+	defaultOptions: {
+		query: {
+			fetchPolicy: "no-cache",
+			errorPolicy: "ignore"
+		},
+		watchQuery: {
+			fetchPolicy: "no-cache",
+			errorPolicy: "all"
+		}
+	}
 });
 export default Client;
