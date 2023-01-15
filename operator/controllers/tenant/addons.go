b08aef70b353d16b5098a9ca78b35067bec67f6b
Django Cass <django@dcas.dev>
2023-01-15 16:19:40 +1100

    [operator] updated addons

--- operator/controllers/tenant/addons.go
+++ operator/controllers/tenant/addons.go
@@ -7,10 +7,10 @@ import (
 )
 
 const (
-	imageDashboardKube = "ghcr.io/vkp-app/addons/dashboard-k8s:1.1.2"
-	imageDashboardOKD  = "ghcr.io/vkp-app/addons/dashboard-okd:1.1.1"
+	imageDashboardKube = "ghcr.io/vkp-app/addons/dashboard-k8s:1.2.0"
+	imageDashboardOKD  = "ghcr.io/vkp-app/addons/dashboard-okd:1.2.0"
 	imagePodInfo       = "ghcr.io/vkp-app/addons/podinfo:1.0.1"
-	imagePromAdapter   = "ghcr.io/vkp-app/addons/prometheus-adapter:1.3.7"
+	imagePromAdapter   = "ghcr.io/vkp-app/addons/prometheus-adapter:1.4.0"
 )
 
 func Addons(tr *v1alpha1.Tenant) []v1alpha1.ClusterAddon {
